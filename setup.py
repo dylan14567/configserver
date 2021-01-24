9a11afd6815655f556c68ae46b52f9b4eca2e883
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-01-23 20:52:12 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="configserver", 
+    version="0.0.1",
+    author="dylan14567",
+    author_email="",
+    description="configure your server with configserver",
+    long_description=README.md,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dylan14567/configserver",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
