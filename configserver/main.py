da7ece05e7d98477c3237af1c7715b80076146f2
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-01-24 15:15:17 -0500

    Update main.py

--- configserver/main.py
+++ configserver/main.py
@@ -1,1 +1,12 @@
+class server:
 
+     def _init_(self):
+            print (Fore.WHITE + "Welcome")
+   
+     def ip (self, ip):
+            self.ip = ip
+            print ("The ip is", ip)
+
+     def port (self, port):
+            self.port = port
+            print ("The port is", port)
