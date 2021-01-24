7cec34c97b88b7362dd9c5a4c96097a22cafc9eb
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-01-23 21:12:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,13 @@
-import setuptools
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="configserver", 
-    version="0.0.1",
-    author="dylan14567",
-    author_email="",
-    description="configure your server with configserver",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/dylan14567/configserver",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    keywords="configserver",
-    install_requires=["colorama"],
-    include_package_data=True,
+from distutils.core import setup
+setup(
+  name = 'configserver',
+  packages = ['configserver'], 
+  version = '0.1',
+  description = 'configure your server with configserver',
+  author = 'dylan14567',
+  author_email = '',
+  url = 'https://github.com/dylan14567/configserver', 
+  download_url = 'https://github.com/{user_name}/{repo}/tarball/0.1',
+  keywords = ['configserver', 'server', 'framework'],
+  classifiers = [],
 )
