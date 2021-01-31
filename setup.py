260249a7349a96df2af4af138d3472fefff8084d
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-01-31 18:04:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
+
+# https://packaging.python.org/distributing/#packaging-your-project
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 setup(
-  name = 'configserver',
-  packages = ['configserver'], 
-  version = '0.1',
-  description = 'configure your server with configserver',
-  author = 'dylan14567',
-  author_email = '',
-  url = 'https://github.com/dylan14567/configserver', 
-  download_url = 'https://github.com/dylan14567/configserver/tarball/1.0',
-  scripts = ['configserver.py'],
-  keywords = ['configserver', 'server', 'framework'],
-  classifiers = [],
+    name = 'configserver',
+    version = '1.0',
+    license='MIT',
+    description = 'Configure your server with configserver',
+    long_description = readme(),
+    author = 'dylan14567',
+    author_email = '',
+    url = 'https://github.com/dylan14567/configserver',
+    scripts = ['configserver'],
+    classifiers = (
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3'
+    )
 )
