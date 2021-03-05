d7e45a4142413a5535ec338a213a12e47be8422b
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-03-05 16:30:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,25 @@
-from setuptools import setup
+import setuptools
 
-# https://packaging.python.org/distributing/#packaging-your-project
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-with open("README.md", "r", encoding="utf-8") as f:
-    README = f.read()
-
-setup(
-    name = 'configserver',
-    packages = ['adminserver'],
-    include_package_data=True,
-    version = '1.7',
-    license='MIT',
-    description = 'Configure your linux server with configserver',
-    long_description=README,
+setuptools.setup(
+    name="configserver", 
+    version="1.7",
+    author="dylan14567",
+    author_email="",
+    description="Configure your linux server and check for vulnerabilities with configserver.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    author = 'dylan14567',
-    author_email = '',
-    url = 'https://github.com/dylan14567/configserver',
-    download_url="https://github.com/dylan14567/configserver/archive/1.7.tar.gz",
-    scripts = ['configserver'],
-    keywords = ['python', 'server', 'configserver', 'admin'], 
-    install_requires=[
-        "colorama",
-        "requests",
+    url="https://github.com/dylan14567/configserver",
+    project_urls={
+        "Bug Tracker": "https://github.com/dylan14567/configserver/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    classifiers = (
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3'
-    )
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
 )
