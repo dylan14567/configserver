52ce12cc5f0ef1cc22e93caaffec1c356b8e4e79
dylan14567 <mundodylan14567dylanmotta78@gmail.com>
2021-03-05 15:36:56 -0500

    Update

--- adminserver/main.py
+++ adminserver/main.py
@@ -10,7 +10,7 @@ class main:
               return
 
        def version(self):
-              print ("1.5")
+              print ("1.6")
 
        def name(self):
               print ("configserver")
