68317813babfa545ce68f255efd5eb4c50c0b618
DYLAN1456732 <55631040+dylan14567@users.noreply.github.com>
2021-03-05 16:05:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 # https://packaging.python.org/distributing/#packaging-your-project
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
 setup(
     name = 'configserver',
     packages = ['adminserver'],
@@ -12,7 +12,8 @@ setup(
     version = '1.7',
     license='MIT',
     description = 'Configure your linux server with configserver',
-    long_description = readme(),
+    long_description=README,
+    long_description_content_type="text/markdown",
     author = 'dylan14567',
     author_email = '',
     url = 'https://github.com/dylan14567/configserver',
